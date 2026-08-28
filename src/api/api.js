d48eddbbf57f724335342060d31b2e5a3fd99c2a
Sanjaydas <sanjaydasps0@gmail.com>
2026-08-28 14:30:42 +0530

    backend db change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://zawara-backend.onrender.com"
+  baseURL: "https://zawara-backend-23bb.onrender.com"
   // baseURL:"http://127.0.0.1:8000/"
 
 });
